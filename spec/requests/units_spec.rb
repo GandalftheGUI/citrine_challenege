1c7e50d5bdea99dddb799f0614d63e8cc14a18d5
Ian Remillard <Doom-Mac@Ians-iMac.local>
2017-12-27 12:42:16 -0800

    added initial example test"

--- spec/requests/units_spec.rb
+++ spec/requests/units_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Units API', type: :request do
-  
+  describe "GET units/si" do
+    context 'given example' do
+      let(:valid_attributes) { { units: 'degree/minute' } }
+
+      before { get '/units/si', params: valid_attributes }
+
+      it 'should convert units' do
+        expect(json['unit_name']).to eq('rad/s')
+      end
+
+      it 'should give the correct multiplication_factor' do
+        expect(json['multiplication_factor']).to eq(0.00029088820867)
+      end
+    end
+  end
 end
