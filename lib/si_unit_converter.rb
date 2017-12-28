18b5344517188a7ddbba5dad5f1e2ff5902a0f66
Ian Remillard <Doom-Mac@Ians-iMac.local>
2017-12-27 16:48:55 -0800

    unit string converter working

--- lib/si_unit_converter.rb
+++ lib/si_unit_converter.rb
@@ -13,7 +13,7 @@ class SiUnitConverter
     'd' => 's',
     '°' => 'rad',
     '\'' => 'rad',
-    '\"' => 'rad',
+    '"' => 'rad',
     'ha' => 'm^2',
     'L' => 'm^3',
     't' => 'kg',
@@ -33,7 +33,7 @@ class SiUnitConverter
     'd' => 86400,
     '°' => (Math::PI/180),
     '\'' => (Math::PI/10800),
-    '\"' => (Math::PI/648000),
+    '"' => (Math::PI/648000),
     'ha' => 10000,
     'L' => 0.001,
     't' => 1000,
@@ -42,7 +42,7 @@ class SiUnitConverter
   attr_accessor :si_unit_string, :input_unit_string, :multiplication_factor
 
   def initialize(input_unit_string)
-    @input_unit_string = input_unit_string.downcase
+    @input_unit_string = input_unit_string.dup
     return unless SiUnitConverter.valid_unit_string?(@input_unit_string)
     build_si_unit_string
     build_si_multiplication_factor
@@ -50,21 +50,30 @@ class SiUnitConverter
 
   #checks that input string contains only acceptable units and symbols
   def self.valid_unit_string?(unit_string)
-    #NOTE: Does not check for balanced parenthesis
-    #checks if string contains only: [a-z] ( ) * / ' " °
-    return false unless unit_string.count("^a-z()*\/\"\'°").zero?
+    #NOTE: Does not check for balanced parenthesis or doulbe operators (e.g. '**' or '/*')
+    #checks if string contains only: { [a-zL] ( ) * / ' " ° }
+
+    return false unless unit_string.count("^a-zL()*\/\"\'°").zero?
+    return false if unit_string.empty?
     #checks if unit string only contains the acceptable
-    units = unit_string.split(/[()*\/]+/).reject(&:empty?)
+    units = unit_string.split(/[\(\)*\/]+/).reject(&:empty?)
     return !units.detect{|unit| UNIT_LOOKUP_TABLE[unit] == nil}
   end
 
   private
 
   def convert_string(lookup_table)
-    lookup_table.keys
+    tokens = @input_unit_string.split(/([\(\)*\/]+)/).reject(&:empty?)
+
+    tokens.each_with_index do |token, index|
+      tokens[index] = lookup_table[token] if lookup_table[token] != nil
+    end
+
+    return tokens.join
   end
 
   def build_si_unit_string
+    @si_unit_string = convert_string(UNIT_LOOKUP_TABLE)
   end
 
   #valid chars: ()*0-9
--- spec/lib/si_unit_converter_spec.rb
+++ spec/lib/si_unit_converter_spec.rb
@@ -8,9 +8,23 @@ describe SiUnitConverter do
       end
     end
 
-    ['bla', 'degree+minute', 'hour minute'].each do |test_string|
+    ['bla', 'degree+minute', 'hour minute', ''].each do |test_string|
       it "should return false for invalid string: '#{test_string}'" do
         expect(SiUnitConverter.valid_unit_string?(test_string)).to be false
+      end
+    end
+  end
+
+  describe 'converted unit string' do
+    test_unit_strings = {
+      'degree/minute' => 'rad/s',
+      '(tonne/day)' => '(kg/s)',
+      '(L*")/min' => '(m^3*rad)/s'
+    }
+
+    test_unit_strings.keys.each do |test_string|
+      it "should correctly convert the given unit string: '#{test_string}'" do
+        expect(SiUnitConverter.new(test_string).si_unit_string).to eq(test_unit_strings[test_string])
       end
     end
   end
